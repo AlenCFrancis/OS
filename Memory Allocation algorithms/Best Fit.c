c74916e9551980b7683dec8b48e612c1702d79c1
Alen C Francis <alencfrancis@gmail.com>
2026-04-14 13:01:47 +0530

    Best Fit memory allocation algorithm

---
+++ Memory Allocation algorithms/Best Fit.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int main()
+{
+    int blockSize[25], originalBlockSize[25];
+    int processSize[25], allocation[25], fragment[25];
+    int numBlocks, numProcesses;
+    int i, j;
+
+    printf("Enter number of memory blocks: ");
+    scanf("%d", &numBlocks);
+
+    printf("Enter number of processes: ");
+    scanf("%d", &numProcesses);
+
+    printf("\nEnter sizes of memory blocks:\n");
+    for(i = 0; i < numBlocks; i++)
+    {
+        printf("Block %d: ", i + 1);
+        scanf("%d", &blockSize[i]);
+        originalBlockSize[i] = blockSize[i];
+    }
+
+    printf("\nEnter sizes of processes:\n");
+    for(i = 0; i < numProcesses; i++)
+    {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &processSize[i]);
+        allocation[i] = -1;
+        fragment[i] = 0;
+    }
+
+    for(i = 0; i < numProcesses; i++)
+    {
+        int bestIndex = -1;
+
+        for(j = 0; j < numBlocks; j++)
+        {
+            if(blockSize[j] >= processSize[i])
+            {
+                if(bestIndex == -1 || blockSize[j] < blockSize[bestIndex])
+                {
+                    bestIndex = j;
+                }
+            }
+        }
+
+        if(bestIndex != -1)
+        {
+            allocation[i] = bestIndex;
+            fragment[i] = blockSize[bestIndex] - processSize[i];
+            blockSize[bestIndex] -= processSize[i];
+        }
+    }
+
+    printf("\nPNo\tPSize\tBNo\tBSize\tFragment\n");
+    for(i = 0; i < numProcesses; i++)
+    {
+        printf("%d\t%d\t", i+1, processSize[i]);
+
+        if(allocation[i] != -1)
+            printf("%d\t%d\t%d\n", allocation[i]+1, originalBlockSize[allocation[i]], fragment[i]);
+        else
+            printf("NA\tNA\tNA\n");
+    }
+
+    return 0;
+}
